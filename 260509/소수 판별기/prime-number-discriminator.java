c20501410e4371238afd924925b7b9a5f879e5d0
Kangmin Kim <bighead0831@naver.com>
2026-05-09 14:44:44 +0900

    [Passed] 소수 판별기 (89ms, 10MB)

---
+++ 260509/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for(int i=1; i<=N; i++) 
+            if(N%i==0) cnt++;
+        System.out.println(cnt==2?"P":"C");
+    }
+}
